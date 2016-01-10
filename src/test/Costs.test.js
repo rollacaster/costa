5271d1ccc79627e8e46f35daf169a7ac0e099f33
Thomas Sojka <thomas@Thomass-MBP.home>
2016-01-10 15:32:22 +0100

    Create integration tests for costs

---
+++ src/test/Costs.test.js
@@ -0,0 +1,55 @@
+import WebSocket from 'ws'
+import { expect } from 'chai'
+
+import { credateCosts } from '../actions'
+
+describe('Cost integration tests', () => {
+  describe('Create new cost', () => {
+    let ws
+
+    beforeEach(() => {
+      ws = new WebSocket('ws://localhost:3000')
+    })
+
+    afterEach(done => {
+      ws.on('close', () => done())
+      ws.close()
+    })
+
+    it('should open a ws connection', (done) => {
+      ws.on('open', () => done())
+    })
+
+    it('should receive the state', done => {
+      const initState = {costs: {}}
+
+      ws.on('message', msg => {
+        expect(JSON.parse(msg)).to.be.deep.equal(initState)
+        done()
+      })
+    })
+
+    it('should receive the updated state', done => {
+      ws.on('open', () => {
+        ws.on('message', msg => {
+          const state = JSON.parse(msg)
+          const hasChildren = Object.keys(state.costs).length > 0
+
+          if (hasChildren) {
+            expect(JSON.parse(msg).costs).to.be.deep.equal({
+              Food: 1
+            })
+            done()
+          }
+        })
+
+        ws.send(JSON.stringify(
+          credateCosts({
+            category: 'Food',
+            cost: 1
+          })
+        ))
+      })
+    })
+  })
+})
