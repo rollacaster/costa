5d88d77305685ad256a1e5d2abb5102535b7f271
Thomas Sojka <thomas.sojka@comsysto.com>
2016-01-16 15:55:37 +0100

    Move static folder

--- src/server/app.js
+++ src/server/app.js
@@ -4,12 +4,10 @@ import bodyParser from 'body-parser'
 
 const app = webServer()
 
-app.use(favicon(__dirname + '/../static/images/favicon.ico'))
+app.use(favicon('static/images/favicon.ico'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(webServer.static('./static'))
-
-app.use('*', (req, res) => res.send('Not implemented yet.'))
+app.use('/', webServer.static('static'))
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500)
