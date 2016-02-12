dcc4d65d32d2ae746c373f302fd0cd8d4996bb29
Thomas Sojka <thomas.sojka@comsysto.com>
2016-02-12 07:13:24 +0100

    Remove router

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
 
 import App from './App'
-import CostForm from './components/CostForm'
 
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path='/' component={App} />
-    <Route path='/form' component={CostForm} />
-  </Router>
-), document.getElementById('content'))
+ReactDOM.render(<App />, document.getElementById('content'))
 
