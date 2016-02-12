25871b3832188d46ef4e2d90097d882e6a044acb
Thomas Sojka <thomas.sojka@comsysto.com>
2016-02-12 07:18:14 +0100

    Improve test readability

--- src/dashboard/components/CostList.spec.jsx
+++ src/dashboard/components/CostList.spec.jsx
@@ -20,32 +20,36 @@ describe('CostList', () => {
     output = renderer.getRenderOutput()
   })
 
-  it('should render a head row', () => {
-    expect(output).to.be.include(
-      <thead _radiumDidResolveStyles>
+  describe('Appearance', () => {
+    it('should render a head row', () => {
+      expect(output).to.be.include(
+        <thead _radiumDidResolveStyles>
+          <tr _radiumDidResolveStyles>
+            <TableHeadText>Category</TableHeadText>
+            <th>Cost</th>
+          </tr>
+        </thead>
+      )
+    })
+
+    it('should render a cost', () => {
+      expect(output).to.be.include(
         <tr _radiumDidResolveStyles>
-          <TableHeadText>Category</TableHeadText>
-          <th>Cost</th>
+          <TableCellText>Food</TableCellText>
+          <td>3.00</td>
         </tr>
-      </thead>
-    )
+      )
+    })
   })
 
-  it('should render a cost', () => {
-    expect(output).to.be.include(
-      <tr _radiumDidResolveStyles>
-        <TableCellText>Food</TableCellText>
-        <td>3.00</td>
-      </tr>
-    )
-  })
-
-  it('should render the total cost', () => {
-    expect(output).to.be.include(
-      <tr _radiumDidResolveStyles>
-        <TableCellText>Total</TableCellText>
-        <td>5.00</td>
-      </tr>
-    )
+  describe('Functionality', () => {
+    it('should compute the total cost', () => {
+      expect(output).to.be.include(
+        <tr _radiumDidResolveStyles>
+          <TableCellText>Total</TableCellText>
+          <td>5.00</td>
+        </tr>
+      )
+    })
   })
 })
