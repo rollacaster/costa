251e199779ad93e00849d82345b9ac86027c70a5
Thomas Sojka <thomas.sojka@comsysto.com>
2016-02-14 21:10:53 +0100

    Remove test warning

--- src/dashboard/App.spec.js
+++ src/dashboard/App.spec.js
@@ -4,6 +4,9 @@ import { createRenderer, expect } from '../test'
 
 import App from './App'
 
+// Removes missing userAgent warning
+global.navigator = {userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2454.85 Safari/537.36'}
+
 describe('App', () => {
   let output
 
