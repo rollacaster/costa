449e1a01c404d3518888c38f7e366e08d2dfcf71
Thomas Sojka <thomas.sojka@comsysto.com>
2016-03-03 19:01:24 +0100

    Add prod config

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,5 +1,12 @@
 let config
 
+const prodConfig = {
+  ws: {
+    url: 'ws://192.168.0.234',
+    port: 3000
+  }
+}
+
 const devConfig = {
   ws: {
     url: 'ws://localhost',
@@ -15,6 +22,9 @@ const testConfig = {
 }
 
 switch (process.env.NODE_ENV) {
+  case 'production':
+    config = prodConfig
+    break
   case 'development':
     config = devConfig
     break
