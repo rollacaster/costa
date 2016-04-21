8604468ae924f1ab857324c5d510adde05cc46b7
Thomas Sojka <thomas.sojka@comsysto.com>
2016-04-21 18:29:51 +0200

    Format code

--- src/dashboard/components/CostDetails.jsx
+++ src/dashboard/components/CostDetails.jsx
@@ -20,33 +20,25 @@ const CostDetails = React.createClass({
     return (
       <div style={{margin: 20}}>
         <LinkButton text='Cost Details' onClick={() => this.setState({isOpen: !isOpen})}/>
-        {
-          isOpen
-          ? <div style={{display: 'flex', flexWrap: 'wrap'}}>
-          {
-            Object.keys(costs).map((month) => (
-              <SelectableButton
-                style={{margin: 5}}
-                key={month}
-                onClick={() => {
-                  if (month === activeMonth) {
-                    this.setState({activeMonth: ''})
-                  } else {
-                    this.setState({activeMonth: month})
-                  }
-                }}
-                selected={month === activeMonth}
-                text={month} />
-            ))
-          }
-          {
-            activeMonth
-            ? <CostList costs={costs[activeMonth]} connection={connection}/>
-            : ''
-          }
-          </div>
-          : ''
-        }
+          {isOpen && (
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+              {Object.keys(costs).map((month) => (
+                <SelectableButton
+                  style={{margin: 5}}
+                  key={month}
+                  onClick={() => {
+                    if (month === activeMonth) {
+                      this.setState({activeMonth: ''})
+                    } else {
+                      this.setState({activeMonth: month})
+                    }
+                  }}
+                  selected={month === activeMonth}
+                  text={month} />
+              ))}
+            {activeMonth && <CostList costs={costs[activeMonth]} connection={connection}/>}
+            </div>
+          )}
       </div>
     )
   }
