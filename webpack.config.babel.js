aefa55df5a6214bfa40a5f7dc6f3c634eeb1e5e8
Thomas Sojka <thomas.sojka@comsysto.com>
2017-05-17 22:15:21 +0200

    Remove jsx file ending

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,9 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: [
-    './src/dashboard/index'
-  ],
+  entry: ['./src/dashboard/index'],
   output: {
     path: path.join(__dirname, 'static/'),
     filename: 'dashboard.js'
@@ -12,23 +10,25 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015']
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js']
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
     process.env.NODE_ENV === 'production'
-      ? new webpack.optimize.UglifyJsPlugin({minimize: true})
+      ? new webpack.optimize.UglifyJsPlugin({ minimize: true })
       : () => {}
   ],
-  devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : 'cheap-module-source-map',
+  devtool: process.env.NODE_ENV !== 'production'
+    ? 'source-map'
+    : 'cheap-module-source-map',
   debug: process.env.NODE_ENV !== 'production'
 }
